c958057c6129632e21eb4ed1714f153c813b6082
Karan2021 <90594223+Karan2021@users.noreply.github.com>
2021-09-15 15:36:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class handson3
+{
+    public static void Main()
+    {
+        string fileName = @"mytest.txt";
+
+        try
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            Console.Write("Create a file in the disk if it is exists");
+            using (FileStream fileStr = File.Create(fileName))
+            {
+                Console.WriteLine("A file created with name mytest.txt");
+            }
+        }
+        catch (Exception MyExcep)
+        {
+            Console.WriteLine(MyExcep.ToString());
+        }
+    }
+}
